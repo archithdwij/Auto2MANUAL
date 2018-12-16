e3df6739eaafd78fd644547cb96594f2c4b3614c
Archithdwij <30730368+codehack31@users.noreply.github.com>
2018-12-16 14:23:07 +0530

    Add files via upload

--- MainActivity.java
+++ MainActivity.java
@@ -1,83 +1,75 @@
-package com.balram.applocker;
+package com.example.kumar.steps;
 
+import android.content.Context;
 import android.content.Intent;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.os.Build;
+import android.os.Handler;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import com.balram.locker.view.AppLocker;
-import com.balram.locker.view.LockActivity;
-import com.balram.locker.utils.Locker;
+public class MainActivity extends AppCompatActivity implements SensorEventListener {
+
+    TextView tv_steps;
+    SensorManager sensorManager;
 
 
-/**
- * Created by Balram Pandey 12/11/16.
- */
 
-public class MainActivity extends LockActivity implements View.OnClickListener {
 
-    private Button btOnOff;
-    private Button btChange;
 
+    Boolean running = false;
+
+    @RequiresApi(api = Build.VERSION_CODES.P)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btOnOff = (Button) findViewById(R.id.bt_on_off);
-        btOnOff.setOnClickListener(this);
+        tv_steps = (TextView) findViewById(R.id.tv_steps);
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
 
-        btChange = (Button) findViewById(R.id.bt_change);
-        btChange.setText(R.string.change_passcode);
-        btChange.setOnClickListener(this);
 
-        updateUI();
+
+
     }
+
     @Override
-    public void onClick(View view) {
-        if (view.equals(btOnOff)) {
-            int type = AppLocker.getInstance().getAppLock().isPasscodeSet() ? Locker.DISABLE_PASSLOCK
-                    : Locker.ENABLE_PASSLOCK;
-            Intent intent = new Intent(this, LockActivity.class);
-            intent.putExtra(Locker.TYPE, type);
-            startActivityForResult(intent, type);
-        } else if (view.equals(btChange)) {
-            Intent intent = new Intent(this, LockActivity.class);
-            intent.putExtra(Locker.TYPE, Locker.CHANGE_PASSWORD);
-            intent.putExtra(Locker.MESSAGE,
-                    getString(R.string.enter_old_passcode));
-            startActivityForResult(intent, Locker.CHANGE_PASSWORD);
+    protected void onResume() {
+        super.onResume();
+        running = true;
+        Sensor countSensor = sensorManager.getDefaultSensor(Sensor.TYPE_STEP_COUNTER);
+        if(countSensor != null)
+        {
+            sensorManager.registerListener(this,countSensor,SensorManager.SENSOR_DELAY_UI);
+            }
+            else
+        {
+            Toast.makeText(this,"sensor not found!",Toast.LENGTH_SHORT).show();
         }
     }
 
     @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-
-        switch (requestCode) {
-            case Locker.DISABLE_PASSLOCK:
-                break;
-            case Locker.ENABLE_PASSLOCK:
-            case Locker.CHANGE_PASSWORD:
-                if (resultCode == RESULT_OK) {
-                    Toast.makeText(this, getString(R.string.setup_passcode),
-                            Toast.LENGTH_SHORT).show();
-                }
-                break;
-            default:
-                break;
-        }
-        updateUI();
+    protected void onPause() {
+        super.onPause();
+        running = false;
     }
 
-    private void updateUI() {
-        if (AppLocker.getInstance().getAppLock().isPasscodeSet()) {
-            btOnOff.setText(R.string.disable_passcode);
-            btChange.setEnabled(true);
-        } else {
-            btOnOff.setText(R.string.enable_passcode);
-            btChange.setEnabled(false);
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if(running){
+            tv_steps.setText(String.valueOf(event.values[0]));
         }
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
     }
 }
